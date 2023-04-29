5a7e3d93657848d66c68f6ac3ca29ef15bca39ed
Om Aute <admin@Satyajeet-MacBook-Pro.local>
2023-04-29 18:14:05 +0530

    first commit

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -17,36 +17,42 @@ function Work() {
         </h1>
         <div className="container-fluid gallery-container text-center">
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
             alt=""
           />
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1656164631668-8673eab87b84?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHNxdWFyZSUyMGRpbWVuc2lvbnMlMjBpbWFnZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
             alt=""
           />
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
             alt=""
           />
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1556742400-b5b7c5121f99?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fHNxdWFyZSUyMGRpbWVuc2lvbnMlMjBpbWFnZXN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
             alt=""
           />
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
             alt=""
           />
           <img
+            data-aos="fade-up"
             className="img-fluid work__image"
             id="work__image_left"
             src="https://images.unsplash.com/photo-1516131206008-dd041a9764fd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGRlc2lnbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60"
